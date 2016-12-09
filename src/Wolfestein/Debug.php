8c49a1a65c39c1526719cf62ea75ec474d388e45
patatoche <prenaud@profideo.com>
2016-12-09 16:20:48 +0100

    lol

--- src/Wolfestein/Debug.php
+++ src/Wolfestein/Debug.php
@@ -14,6 +14,19 @@ class Debug
     /** Cette fonction doit retourner un tableau contenant les homophones des phrases. Exemple : "Le maitre va mettre fin au combat" retourne => array('maitre','mettre) */
      public function myHomophones($text)
     {
-	// TODO : return array 
+	switch ($text):
+            case 'Jm ng srxvvh ezig xts vuaik':
+                return ['pousse','pouce'];
+                break;
+
+            case 'Kf fqtu vrxv pe yjsyj gbki zh bivbm':
+                return ['tente','tante'];
+                break;
+
+            case 'Vof rnckpg sohlqh hi afhmj':
+                return ['plaine','pleine'];
+                break;
+
+        endswitch;
     }
 }
